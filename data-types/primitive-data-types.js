e757df72300107380df9c449243ed45530fe0933
abubakar308 <mdabubakarsiddique789@gmail.com>
2025-07-19 20:51:45 +0600

    primitive-data-types added

---
+++ data-types/primitive-data-types.js
@@ -0,0 +1,9 @@
+// Primitive Data Types Example
+
+const city = "Dhaka";   // String
+const age = 25;         // Number
+const isStudent = true; // Boolean
+let address;            // Undefined
+const email = null;     // Null
+
+console.log(city, age, isStudent, address, email);
